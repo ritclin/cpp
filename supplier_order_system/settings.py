7971472b17d9214da0387ca2999167abf1ebf580
ritclin <Ritclin@gmail.com>
2025-07-18 16:03:42 +0100

    updated settings.py

--- supplier_order_system/settings.py
+++ supplier_order_system/settings.py
@@ -75,10 +75,21 @@ WSGI_APPLICATION = 'supplier_order_system.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': "cppdatabase",
+        'USER': "riteshclinton",
+        'PASSWORD': "RiteshClinton",
+        'HOST': "database-1.csnwgv7yvv0r.us-east-1.rds.amazonaws.com",
+        'PORT':"5432",
     }
 }
 
